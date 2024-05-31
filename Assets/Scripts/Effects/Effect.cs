d3aa1c388c2c7b52e39cc32b4094ce1775c7a76b
Illia Emelianenko <ie.bntu@gmail.com>
2024-06-01 02:07:00 +0300

    Создал скрипты эффектов
    Создал абстрактный класс Effect (ScriptableObject) и два его наследника ContinuousEffect и OneTimeEffect. Создал тестовый эффект - скрипт TestEffect. Закинул в проект текстуры для карточек и эффектов.

---
+++ Assets/Scripts/Effects/Effect.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Effect : ScriptableObject
+{
+    public string Name;
+    [TextArea(1, 3)]
+    public string Description;
+    public Sprite Sprite;
+    public int Level = 0;
+}
---
+++ Assets/Scripts/Effects/TestEffect.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = nameof(TestEffect), menuName = "Effects/ContinuousEffect/" + nameof(TestEffect))]
+public class TestEffect : ContinuousEffect
+{
+    
+}
